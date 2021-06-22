3907c7e331fdb3492b964f2624c16ae35a3f54ef
Brian Henry <BrianHenryIE@gmail.com>
2021-06-21 17:20:51 -0700

    Add loading indicator over the billing fields

--- src/WooCommerce/js/checkout.js
+++ src/WooCommerce/js/checkout.js
@@ -12,7 +12,6 @@
 
         $('body').on('blur change', '#billing_email', function (e) {
 
-            // TODO: Loading indicator.
             // TODO: It fires even if the value hasn't changed since the last time it fired.
 
             // if it's a valid email (taken from checkout.js)
@@ -20,6 +19,15 @@
             var pattern = new RegExp(/^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[0-9a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i); // eslint-disable-line max-len
 
             if (pattern.test($('#billing_email').val())) {
+
+                // Display loading indicator while an account is checked for.
+                $('.woocommerce-billing-fields__field-wrapper').block({
+                    message: null,
+                    overlayCSS: {
+                        background: '#fff',
+                        opacity: 0.6
+                    }
+                });
 
                 // Fire the ajax for update_order_review
                 // (which will check for a user account).
@@ -35,6 +43,9 @@
 
         $( document.body ).bind( 'updated_checkout', function( data ) {
 
+            // Remove the loading indicator.
+            $('.woocommerce-billing-fields__field-wrapper').unblock();
+
             $('#login_button_clicked').remove();
             $('#password_reset_button_clicked').remove();
 
@@ -46,6 +57,15 @@
                 if (e.keyCode == 13) {
 
                     $('#place_order').prepend('<input type="hidden" name="login_button_clicked" id="login_button_clicked" value="clicked" />');
+
+                    // Display loading indicator while logging in.
+                    $('.woocommerce-billing-fields__field-wrapper').block({
+                        message: null,
+                        overlayCSS: {
+                            background: '#fff',
+                            opacity: 0.6
+                        }
+                    });
 
                     $(document.body).trigger('update_checkout');
 
@@ -62,6 +82,15 @@
 
                 $('#place_order').prepend('<input type="hidden" name="login_button_clicked" id="login_button_clicked" value="clicked" />');
 
+                // Display loading indicator while logging in.
+                $('.woocommerce-billing-fields__field-wrapper').block({
+                    message: null,
+                    overlayCSS: {
+                        background: '#fff',
+                        opacity: 0.6
+                    }
+                });
+
                 $(document.body).trigger('update_checkout');
 
             });
@@ -69,6 +98,15 @@
             $('#inline_password_reset_button').on('click', function (e) {
 
                 $('#place_order').prepend('<input type="hidden" name="password_reset_button_clicked" id="password_reset_button_clicked" value="clicked" />');
+
+                // Display loading indicator after password reset clicked.
+                $('.woocommerce-billing-fields__field-wrapper').block({
+                    message: null,
+                    overlayCSS: {
+                        background: '#fff',
+                        opacity: 0.6
+                    }
+                });
 
                 $(document.body).trigger('update_checkout');
 
